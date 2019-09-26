505b1b31da397b35b5c3d8f330a93764af8accd2
Muhammad Syahrun Dalu <55743453+syahrundalu@users.noreply.github.com>
2019-09-26 13:50:58 +0700

    Add files via upload

---
+++ PBOUlang_10116584_Latihan7_Integer.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116584_Latihan7_Integer;
+
+/**
+ *
+ * @author
+ * NIM      : 10116584
+ * NAMA     : Muhammad Syahrun Dalu
+ */
+public class PBOUlang_10116584_Latihan7_Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte  : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int   : " + i);
+        System.out.println("long  : " + l);
+    }
+    
+}
